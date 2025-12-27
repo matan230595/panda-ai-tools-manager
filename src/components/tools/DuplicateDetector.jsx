d0e0dd74ca430381b6bddd3d626bc07d486376a9
matan230595 <matan230595@gmail.com>
2025-12-27 05:57:16 +0000

    File changes

--- src/components/tools/DuplicateDetector.jsx
+++ src/components/tools/DuplicateDetector.jsx
@@ -6,6 +6,34 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import ToolLogo from '@/components/ToolLogo';
 
 export default function DuplicateDetector({ tools, onMerge, onDelete, onClose }) {
+  const calculateSimilarity = (str1, str2) => {
+    const longer = str1.length > str2.length ? str1 : str2;
+    const shorter = str1.length > str2.length ? str2 : str1;
+    
+    if (longer.length === 0) return 1.0;
+    
+    const costs = [];
+    for (let i = 0; i <= longer.length; i++) {
+      let lastValue = i;
+      for (let j = 0; j <= shorter.length; j++) {
+        if (i === 0) {
+          costs[j] = j;
+        } else if (j > 0) {
+          let newValue = costs[j - 1];
+          if (longer.charAt(i - 1) !== shorter.charAt(j - 1)) {
+            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+          }
+          costs[j - 1] = lastValue;
+          lastValue = newValue;
+        }
+      }
+      if (i > 0) costs[shorter.length] = lastValue;
+    }
+    
+    const distance = costs[shorter.length];
+    return (longer.length - distance) / longer.length;
+  };
+
   // זיהוי כפילויות לפי שם דומה או URL זהה
   const duplicates = useMemo(() => {
     const groups = [];
@@ -45,34 +73,6 @@ export default function DuplicateDetector({ tools, onMerge, onDelete, onClose })
 
     return groups;
   }, [tools]);
-
-  const calculateSimilarity = (str1, str2) => {
-    const longer = str1.length > str2.length ? str1 : str2;
-    const shorter = str1.length > str2.length ? str2 : str1;
-    
-    if (longer.length === 0) return 1.0;
-    
-    const costs = [];
-    for (let i = 0; i <= longer.length; i++) {
-      let lastValue = i;
-      for (let j = 0; j <= shorter.length; j++) {
-        if (i === 0) {
-          costs[j] = j;
-        } else if (j > 0) {
-          let newValue = costs[j - 1];
-          if (longer.charAt(i - 1) !== shorter.charAt(j - 1)) {
-            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
-          }
-          costs[j - 1] = lastValue;
-          lastValue = newValue;
-        }
-      }
-      if (i > 0) costs[shorter.length] = lastValue;
-    }
-    
-    const distance = costs[shorter.length];
-    return (longer.length - distance) / longer.length;
-  };
 
   if (duplicates.length === 0) {
     return null;
